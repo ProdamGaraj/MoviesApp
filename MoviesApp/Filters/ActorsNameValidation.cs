e21615619ea6921521c16ef7e702dfe0f0e82521
prodgaraj@yandex.ru <prodgaraj@yandex.ru>
2020-12-19 23:46:10 +0300

    Custom validation added.

---
+++ MoviesApp/Filters/ActorsNameValidation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using MoviesApp.Models;
+
+namespace MoviesApp.Filters
+{
+    public class ActorsNameAtribute : ValidationAttribute
+    {
+        public ActorsNameAtribute()
+        {
+        }
+        public string GetErrorMesage() => "Actors first name and last name must be longer then 4 simbols";
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value.ToString().Length < 4)
+            {
+                return new ValidationResult(GetErrorMesage());
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
--- MoviesApp/ViewModels/InputActorViewModel.cs
+++ MoviesApp/ViewModels/InputActorViewModel.cs
@@ -1,14 +1,17 @@
-﻿using System;
+﻿using MoviesApp.Filters;
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace MoviesApp.ViewModels
 {
     public class InputActorViewModel
     {
+        [ActorsNameAtribute]
         public string FirstName { get; set; }
 
         [DataType(DataType.Date)]
         public DateTime BirthDate { get; set; }
+        [ActorsNameAtribute]
         public string LastName { get; set; }
     }
 }
