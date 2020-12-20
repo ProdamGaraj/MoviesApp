8c6bc2adb6f19bab02d38250270adf32d952d1c3
prodgaraj@yandex.ru <prodgaraj@yandex.ru>
2020-12-20 23:11:00 +0300

    DTO added to api controller.

--- MoviesApp/Controllers/ActorsApiController.cs
+++ MoviesApp/Controllers/ActorsApiController.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MoviesApp.Data;
 using MoviesApp.Models;
+using MoviesApp.Services;
+using MoviesApp.Services.Dto;
 using MoviesApp.ViewModels;
 
 namespace MoviesApp.Controllers
@@ -16,64 +18,48 @@ namespace MoviesApp.Controllers
     [ApiController]
     public class ActorsApiController : ControllerBase
     {
-        private readonly MoviesContext _context;
-        private readonly IMapper _mapper;
+        private readonly IActorService _service;
 
-        public ActorsApiController(MoviesContext context, IMapper mapper)
+        public ActorsApiController(IActorService service, IMapper mapper)
         {
-            _context = context;
-            _mapper = mapper;
+            _service = service;
         }
 
         // GET: api/ActorsApi
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Actor>>> GetActors()
+        public ActionResult<IEnumerable<ActorDto>> GetActors()
         {
-            var actors = await _context.Actors.ToListAsync();
-            return Ok(_mapper.Map<IEnumerable<Actor>, IEnumerable<ActorViewModel>>(actors));
+            return Ok(_service.GetAllActors());
         }
 
         // GET: api/ActorsApi/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<Actor>> GetActor(int id)
+        public ActionResult<Actor> GetActor(int id)
         {
-            var actor = await _context.Actors.FindAsync(id);
+            var actor = _service.GetActor((int)id);
 
             if (actor == null)
             {
                 return NotFound();
             }
 
-            return Ok(_mapper.Map<Actor, ActorViewModel>(actor));
+            return Ok(actor);
         }
 
         // PUT: api/ActorsApi/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutActor(int id, EditActorViewModel viewModel)
+        public IActionResult PutActor(int id, ActorDto editDto)
         {
 
-            var actor = _mapper.Map<EditActorViewModel, Actor>(viewModel);
+            var actor = _service.UpdateActor(editDto);
             actor.Id = id;
-
-            _context.Entry(actor).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!ActorExists(id))
+          
+                if (actor==null)
                 {
                     return NotFound();
                 }
-                else
-                {
-                    throw;
-                }
-            }
 
             return NoContent();
         }
@@ -82,35 +68,23 @@ namespace MoviesApp.Controllers
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
         [HttpPost]
-        public async Task<ActionResult<ActorViewModel>> PostActor(ActorViewModel viewModel)
+        public ActionResult<ActorViewModel> PostActor(ActorDto inputDto)
         {
-            var actor = _mapper.Map<ActorViewModel, Actor>(viewModel);
-
-            await _context.Actors.AddAsync(actor);
-            await _context.SaveChangesAsync();
+            var actor = _service.AddActor(inputDto);
 
             return CreatedAtAction("GetActor", new { id = actor.Id }, actor);
         }
 
         // DELETE: api/ActorsApi/5
         [HttpDelete("{id}")]
-        public async Task<ActionResult<Actor>> DeleteActor(int id)
+        public ActionResult<Actor> DeleteActor(int id)
         {
-            var actor = await _context.Actors.FindAsync(id);
+            var actor = _service.DeleteActor(id);
             if (actor == null)
             {
                 return NotFound();
             }
-
-            _context.Actors.Remove(actor);
-            await _context.SaveChangesAsync();
-
-            return actor;
-        }
-
-        private bool ActorExists(int id)
-        {
-            return _context.Actors.Any(e => e.Id == id);
+            return Ok(actor);
         }
     }
 }
